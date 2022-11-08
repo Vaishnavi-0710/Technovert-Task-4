070a376af2d96b89daa7b4de41cb7fa90202be1a
Vaishnavi Dhayapule <dhayapulevaishnavi07@gmail.com>
2022-11-08 16:34:11 +0530

    commit

--- contact.js
+++ contact.js
@@ -1,15 +1,15 @@
 function clearForm(){
-    var element=document.getElementsByTagName("input");
-    for(var i= 0;i<element.length;i++){
-      element[i].value="";
-    }
+  var element=document.getElementsByTagName("input");
+  for(var i= 0;i<element.length;i++){
+    element[i].value="";
+  }
 }
 function promo(){
   let val=document.getElementById("state").value;
   if(val){
-      document.getElementById("promocode").value= val+" - PROMO";
+    document.getElementById("promocode").value= val+" - PROMO";
   }
   else{
-      document.getElementById("promocode").value="";
+    document.getElementById("promocode").value="";
   }
 }
